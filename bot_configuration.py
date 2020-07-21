e4b87618bf013c5e95c3e2aa770fc74b42b62c56
KirSupi <kirsupi@gmail.com>
2020-07-21 19:18:18 +0300

    новый токен бота

--- bot_configuration.py
+++ bot_configuration.py
@@ -1,3 +1,3 @@
-TOKEN = "1124731420:AAGQa5H0sNRbumQJI2pqNJmtpct97Hhcgn8"
+TOKEN = "1357678776:AAF6cR8EMuUSGGRKCLj5I6m_ugemWXhdRgI"
 request_ = {'read_timeout': 1000, 'connect_timeout': 1000323}
 group_with_video_id = "-368870653"
